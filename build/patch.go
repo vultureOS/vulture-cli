ead37478d75e0d9876b28746cd7fa4d34dd95f30
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-26 19:08:13 +0530

    build: patching

--- build/patch.go
+++ build/patch.go
@@ -3,9 +3,11 @@ package build
 import (
 	"bytes"
 	"encoding/json"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 	"text/template"
 )
 
@@ -16,9 +18,9 @@ const (
 )
 
 var (
-	vultureosImportTpl = template.Must(template.New("pranaos").Parse(`
-	// + build pranaos
-	
+	vultureosImportTpl = template.Must(template.New("vultureos").Parse(`
+	//+build vultureos
+
 	package {{.name}}
 	import _ "github.com/vultureOS/vulture"
 	`))
@@ -42,12 +44,35 @@ type buildOverlay struct {
 	Replace map[string]string
 }
 
-func (b *Builder) pranaosImportFile() string {
+func (b *Builder) vultureosImportFile() string {
 	return filepath.Join(b.basedir, vultureosImportFile)
 }
 
 func (b *Builder) overlayFile() string {
 	return filepath.Join(b.basedir, overlayFile)
+}
+
+func (b *Builder) buildPrepare() error {
+	var err error
+
+	if !b.modHasvultureos() {
+		log.Printf("vultureos not found in go.mod")
+		err = b.editGoMod()
+		if err != nil {
+			return err
+		}
+	}
+
+	err = b.writeImportFile(b.vultureosImportFile())
+	if err != nil {
+		return err
+	}
+
+	err = writeOverlayFile(b.overlayFile(), vultureosImportFile, b.vultureosImportFile())
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func writeOverlayFile(overlayFile, dest, source string) error {
@@ -60,7 +85,7 @@ func writeOverlayFile(overlayFile, dest, source string) error {
 	return os.WriteFile(overlayFile, buf, 0644)
 }
 
-func (b *Builder) readGoModule() (*gomodule, error) {
+func (b *Builder) readGomodule() (*gomodule, error) {
 	var buf bytes.Buffer
 	cmd := exec.Command(b.gobin(), "mod", "edit", "-json")
 	cmd.Stdout = &buf
@@ -68,12 +93,76 @@ func (b *Builder) readGoModule() (*gomodule, error) {
 	if err != nil {
 		return nil, err
 	}
-
 	var mod gomodule
 	err = json.Unmarshal(buf.Bytes(), &mod)
 	if err != nil {
 		return nil, err
 	}
-
 	return &mod, nil
+}
+
+func (b *Builder) modHasvultureos() bool {
+	if b.currentModulePath() == vultureosModulePath {
+		return true
+	}
+
+	mods, err := b.readGomodule()
+	if err != nil {
+		panic(err)
+	}
+	for _, mod := range mods.Require {
+		if mod.Path == vultureosModulePath {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *Builder) editGoMod() error {
+	getPath := vultureosModulePath
+	if b.cfg.vultureOSVersion != "" {
+		getPath = getPath + "@" + b.cfg.vultureOSVersion
+	}
+	log.Printf("go get %s", getPath)
+	env := []string{
+		"GOOS=linux",
+		"GOARCH=amd64",
+	}
+	env = append(env, os.Environ()...)
+	cmd := exec.Command(b.gobin(), "get", getPath)
+	cmd.Env = env
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func (b *Builder) currentPkgName() string {
+	out, err := exec.Command(b.gobin(), "list", "-f", `{{.Name}}`).CombinedOutput()
+	if err != nil {
+		log.Panicf("get current package name:%s", out)
+	}
+	return strings.TrimSpace(string(out))
+}
+
+func (b *Builder) currentModulePath() string {
+	out, err := exec.Command(b.gobin(), "list", "-f", `{{.Module.Path}}`).CombinedOutput()
+	if err != nil {
+		log.Panicf("get current module path:%s", out)
+	}
+	return strings.TrimSpace(string(out))
+
+}
+
+func (b *Builder) writeImportFile(fname string) error {
+	pkgname := b.currentPkgName()
+	var rawFile bytes.Buffer
+	err := vultureosImportTpl.Execute(&rawFile, map[string]interface{}{
+		"name": pkgname,
+	})
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile(fname, rawFile.Bytes(), 0644)
+	return err
 }
