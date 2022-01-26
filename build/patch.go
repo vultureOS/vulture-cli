d40145048a7c4de1ec028b53e0c8f6e888797279
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-26 18:53:37 +0530

    build: patch
    readGoModule + writeOverlayFile

--- build/patch.go
+++ build/patch.go
@@ -1,6 +1,13 @@
 package build
 
-import "text/template"
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"text/template"
+)
 
 const (
 	vultureosModulePath = "github.com/vultureOS/vulture"
@@ -29,4 +36,44 @@ type gomodule struct {
 	Exclude interface{} `json:"Exclude"`
 	Replace interface{} `json:"Replace"`
 	Retract interface{} `json:"Retract"`
+}
+
+type buildOverlay struct {
+	Replace map[string]string
+}
+
+func (b *Builder) pranaosImportFile() string {
+	return filepath.Join(b.basedir, vultureosImportFile)
+}
+
+func (b *Builder) overlayFile() string {
+	return filepath.Join(b.basedir, overlayFile)
+}
+
+func writeOverlayFile(overlayFile, dest, source string) error {
+	overlay := buildOverlay{
+		Replace: map[string]string{
+			dest: source,
+		},
+	}
+	buf, _ := json.Marshal(overlay)
+	return os.WriteFile(overlayFile, buf, 0644)
+}
+
+func (b *Builder) readGoModule() (*gomodule, error) {
+	var buf bytes.Buffer
+	cmd := exec.Command(b.gobin(), "mod", "edit", "-json")
+	cmd.Stdout = &buf
+	err := cmd.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	var mod gomodule
+	err = json.Unmarshal(buf.Bytes(), &mod)
+	if err != nil {
+		return nil, err
+	}
+
+	return &mod, nil
 }
