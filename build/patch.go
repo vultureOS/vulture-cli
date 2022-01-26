0ef282c87c7ce936b0c04fee10ada01ab3def33e
Krisna Pranav <krisna.pranav@gmail.com>
2022-01-26 18:49:20 +0530

    build: patch
    go module + vultureOS template file + vultureOS Module path + vultureOS
    Import File + overlayFile

--- build/patch.go
+++ build/patch.go
@@ -1,3 +1,32 @@
 package build
 
-const ()
+import "text/template"
+
+const (
+	vultureosModulePath = "github.com/vultureOS/vulture"
+	vultureosImportFile = "import_vultureos.go"
+	overlayFile         = "overlay.json"
+)
+
+var (
+	vultureosImportTpl = template.Must(template.New("pranaos").Parse(`
+	// + build pranaos
+	
+	package {{.name}}
+	import _ "github.com/vultureOS/vulture"
+	`))
+)
+
+type gomodule struct {
+	Module struct {
+		Path string `json:"Path"`
+	} `json:"Module"`
+	Go      string `json:"Go"`
+	Require []struct {
+		Path    string `json:"Path"`
+		Version string `json:"Version"`
+	} `json:"Require"`
+	Exclude interface{} `json:"Exclude"`
+	Replace interface{} `json:"Replace"`
+	Retract interface{} `json:"Retract"`
+}
